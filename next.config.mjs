8c04f99ad03774479e0f6049c2177d6b733ca015
shouta0715 <Myheroes0715@gmail.com>
2024-03-30 23:21:20 +0900

    add: @next/bundle-analyzer and setting next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,19 @@
-/** @type {import('next').NextConfig} */
-
 import path from "path";
+import withBundleAnalyzer from "@next/bundle-analyzer";
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
   sassOptions: {
     includePaths: [path.join(new URL(import.meta.url).pathname, "styles")],
   },
 };
 
-export default nextConfig;
+export default withBundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+})(nextConfig);
