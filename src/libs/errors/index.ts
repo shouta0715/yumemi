80a198aa7cd356959ecb542aea3e113078ecdaf4
shouta0715 <Myheroes0715@gmail.com>
2024-03-31 11:35:52 +0900

    fix: custom error use custom fetch

--- src/libs/errors/index.ts
+++ src/libs/errors/index.ts
@@ -21,14 +21,24 @@ export type Error = {
 export class HttpError extends Error {
   message: ErrorsMessage;
 
-  constructor(public status: ErrorType) {
-    super();
+  originMessage: string | null;
+
+  constructor(
+    public status: ErrorType,
+    originMessage?: string | null
+  ) {
+    super(errors[status].message);
     this.message = errors[status].message;
     this.status = status;
+    this.originMessage = originMessage || null;
   }
 
   throwMessage() {
-    return { message: this.message, status: this.status };
+    return {
+      message: this.message,
+      status: this.status,
+      originMessage: this.originMessage,
+    };
   }
 }
 
@@ -68,6 +78,25 @@ export class InternalServerError extends HttpError {
   }
 }
 
+export const getErrorStatus = (status: number): ErrorType => {
+  switch (status) {
+    case 400:
+      return 400;
+    case 401:
+      return 401;
+    case 403:
+      return 403;
+    case 404:
+      return 404;
+    case 429:
+      return 429;
+    case 500:
+      return 500;
+    default:
+      return 500;
+  }
+};
+
 export const throwHttpErrorFromStatus = (status: ErrorType | number): never => {
   switch (status) {
     case 400:
@@ -92,41 +121,41 @@ export const handleApiError = ({ error }: { error: unknown }) => {
     const status = 400;
     const { message } = errors[status];
 
-    return { message, status };
+    return { message, status, originMessage: error.originMessage };
   }
 
   if (error instanceof UnauthorizedError) {
     const status = 401;
     const { message } = errors[status];
 
-    return { message, status };
+    return { message, status, originMessage: error.originMessage };
   }
 
   if (error instanceof ForbiddenError) {
     const status = 403;
     const { message } = errors[status];
 
-    return { message, status };
+    return { message, status, originMessage: error.originMessage };
   }
 
   if (error instanceof NotFoundError) {
     const status = 404;
     const { message } = errors[status];
 
-    return { message, status };
+    return { message, status, originMessage: error.originMessage };
   }
 
   if (error instanceof TooManyRequestsError) {
     const status = 429;
     const { message } = errors[status];
 
-    return { message, status };
+    return { message, status, originMessage: error.originMessage };
   }
 
   if (error instanceof HttpError) {
     const { status, message } = error.throwMessage();
 
-    return { message, status };
+    return { message, status, originMessage: error.originMessage };
   }
 
   const status = 500;
