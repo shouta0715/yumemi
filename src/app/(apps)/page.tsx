c1517d9b866512a38ef1872643cfa003708fc095
shouta0715 <Myheroes0715@gmail.com>
2024-04-01 21:07:35 +0900

    add: apps layout and prefectures component

--- src/app/(apps)/page.tsx
+++ src/app/(apps)/page.tsx
@@ -1,3 +1,17 @@
-export default function Page() {
-  return <div>set up</div>;
+import { fetchPrefectures } from "@/app/(apps)/_features/prefectures/api";
+import { Prefectures } from "@/app/(apps)/_features/prefectures/components";
+import { SearchParams } from "@/libs/types/next";
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: SearchParams;
+}) {
+  const { result: prefectures } = await fetchPrefectures();
+
+  return (
+    <div>
+      <Prefectures prefectures={prefectures} searchParams={searchParams} />
+    </div>
+  );
 }
