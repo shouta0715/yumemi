b6f169296a2bbbfdd248e52d3ce8537b51b3f47f
shouta0715 <Myheroes0715@gmail.com>
2024-04-01 21:05:27 +0900

    add: component prefecture checkbox

---
+++ src/app/(apps)/_features/checkbox/components/index.stories.tsx
@@ -0,0 +1,81 @@
+import { Meta, StoryObj } from "@storybook/react";
+
+import { expect, userEvent, within } from "@storybook/test";
+import { PrefectureCheckbox } from "@/app/(apps)/_features/checkbox/components";
+
+export default {
+  component: PrefectureCheckbox,
+  title: "apps/prefectures/preference-checkbox",
+  tags: ["autdocs"],
+} satisfies Meta<typeof PrefectureCheckbox>;
+
+type Story = StoryObj<typeof PrefectureCheckbox>;
+
+export const Default: Story = {
+  args: {
+    defaultPrefCodes: ["13"],
+    prefecture: {
+      prefCode: 13,
+      prefName: "東京都",
+    },
+  },
+};
+
+export const Checked: Story = {
+  parameters: {
+    nextjs: {
+      navigation: {
+        pathname: "/",
+        query: {
+          prefCodes: "13",
+        },
+      },
+    },
+  },
+
+  args: {
+    defaultPrefCodes: ["13"],
+    prefecture: {
+      prefCode: 13,
+      prefName: "東京都",
+    },
+  },
+
+  play: async ({ canvasElement, parameters }) => {
+    const canvas = within(canvasElement);
+    const checkbox = canvas.getByRole("checkbox");
+
+    expect(checkbox).toBeChecked();
+
+    await userEvent.click(checkbox);
+
+    await expect(parameters.nextjs.navigation.push).toBeCalledTimes(1);
+
+    expect(checkbox).not.toBeChecked();
+  },
+};
+
+export const NotChecked: Story = {
+  args: {
+    defaultPrefCodes: [],
+    prefecture: {
+      prefCode: 13,
+      prefName: "東京都",
+    },
+  },
+
+  play: async ({ canvasElement, parameters }) => {
+    const canvas = within(canvasElement);
+    const checkbox = canvas.getByRole("checkbox");
+
+    expect(checkbox).not.toBeChecked();
+
+    await userEvent.click(checkbox);
+
+    await expect(parameters.nextjs.navigation.push).toHaveBeenCalledWith(
+      "/?prefCodes=13"
+    );
+
+    expect(checkbox).toBeChecked();
+  },
+};
