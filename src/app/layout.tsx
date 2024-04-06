5daef8bdfd693e4d72f3ea9f27ffcba3868361dc
shouta0715 <Myheroes0715@gmail.com>
2024-04-06 17:15:46 +0900

    add: metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,14 @@
 import clsx from "clsx";
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 import "@/styles/global/index.scss";
 
 import styles from "./layout.module.scss";
+import { basicMetadata } from "@/libs/meta";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: {
-    default: "Poplus",
-    template: "%s | Poplus",
-  },
-  description: "都道府県別の人口推移をグラフで表示します。",
-};
+export const metadata = basicMetadata;
 
 export default function RootLayout({
   children,
