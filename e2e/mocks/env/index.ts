d40ec41d4ff3ade17b6f9cf7555a06b95dba6f58
shouta0715 <Myheroes0715@gmail.com>
2024-04-07 23:31:17 +0900

    WIP: fix env

--- e2e/mocks/env/index.ts
+++ e2e/mocks/env/index.ts
@@ -2,19 +2,8 @@ type Env = "URL" | "KEY";
 export const getTestEnv = (env: Env): string => {
   const { API_URL, API_KEY } = process.env;
 
-  if (API_URL) console.log("Loaded API_URL from .env", API_URL);
-  if (API_KEY) console.log("Loaded API_KEY from .env");
-
-  if (!API_URL) {
-    console.error("API_URL is not defined");
-
-    throw new Error("API_URL is not defined");
-  }
-  if (!API_KEY) {
-    console.error("API_KEY is not defined");
-
-    throw new Error("API_KEY is not defined");
-  }
+  if (!API_URL) throw new Error("API_URL is not defined");
+  if (!API_KEY) throw new Error("API_KEY is not defined");
 
   if (env === "URL") return API_URL;
 
