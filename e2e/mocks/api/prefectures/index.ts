36f1044947d9416adc411fced54bb163f097058f
shouta0715 <Myheroes0715@gmail.com>
2024-04-07 18:08:51 +0900

    add: mock prefectures api

---
+++ e2e/mocks/api/prefectures/index.ts
@@ -0,0 +1,44 @@
+import {
+  RequestHandler,
+  rest,
+} from "next/experimental/testmode/playwright/msw";
+import { getTestEnv } from "../../env";
+import { mockPrefectures } from "@/app/(apps)/_features/prefectures/mocks";
+import { FetchPrefecturesResponse } from "@/libs/types/api/prefectures";
+
+type MockPrefecturesHandlerParams = {
+  status?: number;
+  delay?: number;
+  response?: FetchPrefecturesResponse;
+};
+
+const basicResponse = {
+  message: null,
+  result: mockPrefectures,
+};
+
+const errorResponse = {
+  statusCode: 403,
+  message: "Forbidden",
+  description: "You don't have permission to access this resource.",
+};
+
+export const mockPrefecturesHandler: (
+  params?: MockPrefecturesHandlerParams
+) => RequestHandler = (params) => {
+  return rest.get(`${getTestEnv("URL")}/api/v1/prefectures`, (_, res, ctx) => {
+    const { status = 200, delay = 0, response = basicResponse } = params ?? {};
+
+    if (status >= 503) {
+      return res.networkError("Failed to connect");
+    }
+
+    const data = status >= 400 ? errorResponse : response;
+
+    return res(
+      ctx.status(status),
+      ctx.delay(delay),
+      ctx.json<FetchPrefecturesResponse>(data)
+    );
+  });
+};
